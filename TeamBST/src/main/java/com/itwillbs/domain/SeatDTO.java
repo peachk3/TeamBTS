ab9329656991a50135c1d7b9abcc5f6e9cc0b57a
peach <peach@LAPTOP-HVM065VU>
2024-06-08 23:32:35 +0900

    DTO 작성

---
+++ TeamBST/src/main/java/com/itwillbs/domain/SeatDTO.java
@@ -0,0 +1,13 @@
+package com.itwillbs.domain;
+
+import lombok.Data;
+
+@Data
+public class SeatDTO {
+	
+	private long seat_id;
+	private int seat_now;
+	private int seat_num;
+	private int zone_id;
+	
+}
