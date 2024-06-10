bf2f6996b6d52a6d9e9872b52a31ac493332cb2e
peachk3 <doo062991@gmail.com>
2024-06-10 14:51:39 +0900

    #11 DTO 수정 및 재업로드

--- TeamBST/src/main/java/com/itwillbs/domain/SeatDTO.java
+++ TeamBST/src/main/java/com/itwillbs/domain/SeatDTO.java
@@ -6,8 +6,9 @@ import lombok.Data;
 public class SeatDTO {
 	
 	private long seat_id;
-	private int seat_now;
+	private int seat_row;
 	private int seat_num;
+	private boolean seat_booked;
 	private int zone_id;
 	
 }
